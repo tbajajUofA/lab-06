50348b480a9a4c60d2ca67f16c5c34c417ac1221
tbajajUofA <tbajaj@ualberta.ca>
2025-10-15 13:41:06 -0600

    coded

---
+++ ListyCity/app/src/main/java/com/example/listycity/City.java
@@ -0,0 +1,29 @@
+package com.example.listycity;
+
+/**
+ * This is a class that defines a City
+ */
+public class City implements Comparable {
+    private String city;
+    private String province;
+
+    public City(String city, String province) {
+        this.city = city;
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    @Override
+    public int compareTo(Object o){
+        City city = (City) o;
+        return this.city.compareTo(city.getCity());
+    }
+
+}
